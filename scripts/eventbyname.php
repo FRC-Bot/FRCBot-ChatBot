c4810361cfbb1952f894ea1eae900e304ba39afe
technonerdz <technonerdzfg@gmail.com>
2016-11-01 19:19:39 -0400

    Update eventbyname.php

--- scripts/eventbyname.php
+++ scripts/eventbyname.php
@@ -1,6 +1,7 @@
 <?php
   $evname = $_GET["eventname"];
-  
+  $evname = str_replace(filter_var($evname, FILTER_SANITIZE_NUMBER_INT), '', $evname);
+
   $json_string = file_get_contents("https://www.googleapis.com/customsearch/v1?q={$evname}&cx=**link to Google custom search***&siteSearch=thebluealliance.com&key=AIzaSyDAWhaM5K2OOVwJ5dxA2mp6s9ntqN3fjJw");
   $parsed_json = json_decode($json_string);
   
